e4580be3d6955cff6cd3008d7ae4cba8b7bbdccf
Leandro Gomez <leandro.grotnig@gmail.com>
2025-04-22 12:17:48 -0300

    Adds cloudflare pages integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,20 @@ import tailwindcss from "@tailwindcss/vite";
 import { fileURLToPath } from "url";
 import path from "path";
 
+import cloudflare from "@astrojs/cloudflare";
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()],
-        resolve: {
-            alias: {
-                "@": path.resolve(__dirname, "./src"),
-            },
-        },
-    },
+  vite: {
+      plugins: [tailwindcss()],
+      resolve: {
+          alias: {
+              "@": path.resolve(__dirname, "./src"),
+          },
+      },
+  },
+
+  adapter: cloudflare(),
 });
